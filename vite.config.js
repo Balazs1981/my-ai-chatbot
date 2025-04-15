ec7972655fdda6dfa196c9f0933c25c71e81ef20
unknown <vodasux@freemail.hu>
2025-04-15 16:28:27 +0200

    Fix: környezeti változó beillesztése

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   define: {
     'import.meta.env.VITE_OPENROUTER_KEY': JSON.stringify(process.env.VITE_OPENROUTER_KEY)
   }
-};
+});
