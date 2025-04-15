401c911f48d079b6e56732236cb768a1ff80e09e
unknown <vodasux@freemail.hu>
2025-04-15 16:19:45 +0200

    javitas3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    outDir: 'dist'
+// vite.config.js
+export default {
+  define: {
+    'import.meta.env.VITE_OPENROUTER_KEY': JSON.stringify(process.env.VITE_OPENROUTER_KEY)
   }
-});
+};
